8b064f25706605c4ac4bb5a103251490b9d37356
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2021-10-27 20:43:03 +0200

    Cleanup code

--- ckanext/datavic_odp_schema/plugin.py
+++ ckanext/datavic_odp_schema/plugin.py
@@ -2,20 +2,16 @@ import logging
 
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-import re
 
 import ckanext.datavic_odp_schema.helpers as helpers
 
-_t = toolkit._
 
-log1 = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
+
 
 class DatavicODPSchema(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IConfigurer)
-    # plugins.implements(plugins.IDatasetForm, inherit=True)
-    plugins.implements(plugins.IRoutes, inherit=True)
-    plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IBlueprint)
 
     # IBlueprint
@@ -25,7 +21,6 @@ class DatavicODPSchema(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        # toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('webassets', 'datavic-odp-schema')
         toolkit.add_ckan_admin_tab(config_, 'ckanadmin_organisations.admin', 'Org. tools')
 
@@ -52,27 +47,3 @@ class DatavicODPSchema(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
             'get_formats': helpers.get_formats,
             'dataset_fields': helpers.dataset_fields('dataset')
         }
-
-    # IDatasetForm interface ##
-    def is_fallback(self):
-        '''
-        Return True to register this plugin as the default handler for
-        package types not handled by any other IDatasetForm plugin.
-        '''
-        return True
-
-    def package_types(self):
-        '''
-        This plugin doesn't handle any special package types, it just
-        registers itself as the default (above).
-        '''
-        return []
-
-    def after_show(self, context, pkg_dict):
-        """
-        DATAVIC-232: Remove custodian details before showing or indexing dataset
-        """
-        pkg_dict.pop('maintainer', None)
-        pkg_dict.pop('maintainer_email', None)
-
-        return pkg_dict
