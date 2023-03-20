baba9cd4781c84c9587be0e149122cfdfca69eeb
ajmal <dev@ajmalaju.com>
2023-03-20 11:43:59 +0530

    SXDEDPCXZIC-92/added command for clearing harvester entries

--- ckanext/datavic_odp_schema/cli/__init__.py
+++ ckanext/datavic_odp_schema/cli/__init__.py
@@ -1,23 +1,31 @@
 import logging
 
 import click
-
 from ckan.plugins.toolkit import enqueue_job
-
 from ckanext.datavic_odp_schema import jobs
 
+from . import maintain
 
 log = logging.getLogger(__name__)
 
 
-@click.command("ckan-job-worker-monitor")
+@click.group()
+def datavic_odp():
+    """datavic odp management commands."""
+    pass
+
+
+@datavic_odp.command("ckan-job-worker-monitor")
 def ckan_worker_job_monitor():
     try:
         enqueue_job(jobs.ckan_worker_job_monitor, title="CKAN job worker monitor")
-        click.secho("CKAN job worker monitor added to worker queue", fg="green")
+        click.secho(u"CKAN job worker monitor added to worker queue", fg=u"green")
     except Exception as e:
         log.error(e)
 
 
+datavic_odp.add_command(maintain.maintain)
+
+
 def get_commands():
-    return [ckan_worker_job_monitor]
+    return [datavic_odp]
