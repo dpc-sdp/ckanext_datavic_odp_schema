c0e260decc360abea8016f29949f1ee01f3a22af
ajmal <dev@ajmalaju.com>
2023-04-03 10:55:18 +0530

    SXDEDPCXZIC-13/ fix 2.10 updates

--- ckanext/datavic_odp_schema/views/__init__.py
+++ ckanext/datavic_odp_schema/views/__init__.py
@@ -2,5 +2,8 @@ from .odp_dataset import odp_dataset
 from .sitemap import sitemap
 from .odp_api import odpapi
 
-def get_blueprints():
-    return [odp_dataset, sitemap, odpapi]
+__all__ = [
+    "odp_dataset",
+    "sitemap",
+    "odpapi",
+]
