440f7ac05fa086d88123daf6c02e83dd4f91072b
amorev <aleksey.morev@linkdigital.com.au>
2023-07-03 00:00:21 +0300

    SXDEDPCXZIC-181 / fix trash page blocker

--- ckanext/datavic_odp_schema/cli.py
+++ ckanext/datavic_odp_schema/cli.py
@@ -1,8 +1,11 @@
 import logging
+from typing import List
 
 import click
 
 from ckan.plugins.toolkit import enqueue_job
+import ckan.model as model
+import ckan.logic as logic
 
 from ckanext.datavic_odp_schema import jobs
 
@@ -19,5 +22,35 @@ def ckan_worker_job_monitor():
         log.error(e)
 
 
+@click.command(u"purge-deleted-datasets", short_help=u"Purge deleted datasets")
+@click.argument(u"type")
+def purge_deleted_datasets(type: str):
+    """Removes deleted datasets of certain type from db entirely
+
+    Args:
+        type (str): type of datasets to be purged
+    """
+    log.info(f"Searching for deleted datasets of <{type}> type...")
+
+    datasets = model.Session.query(model.Package.id, model.Package.title)\
+        .filter(model.Package.type == type)\
+        .filter(model.Package.state == u"deleted").all()
+
+    if not datasets:
+        click.secho(f"No datasets of <{type}> type were founded", fg="green")
+    else:
+        click.secho(
+            f"{len(datasets)} deleted datasets of <{type}> type were founded.",
+            fg="green"
+        )
+        for dataset in datasets:
+            site_user = logic.get_action(u'get_site_user')({u'ignore_auth': True}, {})
+            context = {u'user': site_user[u'name'], u'ignore_auth': True}
+            logic.get_action(u'dataset_purge')(context, {u'id': dataset[0]})
+            click.secho(f"{dataset[0]} '{dataset[1]}' purged", fg="yellow")
+
+        click.secho("Done.", fg="green")
+
+
 def get_commands():
-    return [ckan_worker_job_monitor]
+    return [ckan_worker_job_monitor, purge_deleted_datasets]
