7bef7431a42f43097c7d84c37641ad819e633aa8
Oleksandr Cherniavskyi <cherniavskyi.alexandr@linkdigital.com.au>
2023-08-18 10:38:54 +0300

    SXDEDPCXZIC-206 / Improve historical data view

--- ckanext/datavic_odp_schema/views/odp_dataset.py
+++ ckanext/datavic_odp_schema/views/odp_dataset.py
@@ -2,11 +2,9 @@ import logging
 
 from flask import Blueprint
 
-import ckan.model as model
 import ckan.plugins.toolkit as tk
-
 import ckan.views.api as api
-from ckan.views.dataset import _get_package_type, _setup_template_variables
+from ckan import types
 
 from ckanext.datavic_odp_theme import helpers
 
@@ -15,27 +13,21 @@ log = logging.getLogger(__name__)
 odp_dataset = Blueprint("odp_dataset", __name__)
 
 
-@odp_dataset.route("/dataset/<id>/historical")
-def historical(id):
-    context = {
-        "model": model,
-        "session": model.Session,
-        "user": tk.g.user or tk.g.author,
-        "for_view": True,
-        "auth_user_obj": tk.g.userobj,
-    }
+@odp_dataset.route("/<package_type>/<package_id>/historical")
+def historical(package_type: str, package_id: str):
+    context: types.Context = tk.fresh_context({})
+
+    data_dict = {"id": package_id}
 
     try:
-        pkg_dict = tk.get_action("package_show")(context, {"id": id})
+        pkg_dict = tk.get_action("package_show")(context, data_dict)
+
     except tk.ObjectNotFound:
         return tk.abort(404, tk._("Dataset not found"))
     except tk.NotAuthorized:
-        return tk.abort(401, tk._("Unauthorized to read package {}").format(id))
+        return tk.abort(401, tk._(f"Unauthorized to read package {package_id}"))
 
-    return tk.render(
-        "package/read_historical.html",
-        {"pkg_dict": pkg_dict, "pkg": context["package"]},
-    )
+    return tk.render("package/read_historical.html", {"pkg_dict": pkg_dict})
 
 
 @odp_dataset.route("/api/action/format_list")
