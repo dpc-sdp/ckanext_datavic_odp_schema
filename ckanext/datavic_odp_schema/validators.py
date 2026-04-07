ec3be246577c7b10069a8685985675a8a0d7e8c5
Awang Setyawan <awang.setyawan@salsa.digital>
2026-04-07 16:02:15 +0700

    [DATAVIC-911] updated schema, resource form error message, removed unsed template

--- ckanext/datavic_odp_schema/validators.py
+++ ckanext/datavic_odp_schema/validators.py
@@ -1,6 +1,5 @@
 from ckan.plugins import toolkit as tk
-from ckan.types import Context, FlattenDataDict, FlattenErrorDict, FlattenKey
-from ckanext.scheming.validation import register_validator, scheming_validator
+from ckanext.scheming.validation import register_validator
 
 
 @register_validator
@@ -14,11 +13,12 @@ def datavic_filesize_validator(key, data, errors, context):
                 "Enter file size in bytes (numeric values only), or leave blank"
             )
 
+
 @register_validator
 def datavic_tag_string_convert(key, data, errors, context):
     """
     Validates the tag_string field and converts into tags only if coming from the dataset form, not the resource form
     """
-    if context.get('save') == True:
+    if context.get('save'):
         tk.get_validator('not_empty')(key, data, errors, context)
         tk.get_validator('tag_string_convert')(key, data, errors, context)
